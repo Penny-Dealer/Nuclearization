bb03d4a365940cc27acd604c929f0045cbadeaa1
Leopoldo Gastel <chunkeypro@leopoldos-mbp.lan>
2022-03-16 13:34:21 -0700

    heroku setup

---
+++ config/env/production/server.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+    url: env("https://nuclearization.herokuapp.com/"),
+    proxy: true,
+    app: {
+      keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    },
+  });
+  w
